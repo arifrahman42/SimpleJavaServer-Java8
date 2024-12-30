f79d3bc832404659046a92ac7f138f0d6d13c34f
Arif Rahman Habibie <arifrahman42.mail@protonmail.com>
2024-12-30 15:42:17 +0700

    Adding date and time in Indonesian

--- src/HelloWorldServer.java
+++ src/HelloWorldServer.java
@@ -1,6 +1,10 @@
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.servlet.*;
 import java.io.IOException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.servlet.http.*;
 
 public class HelloWorldServer {
@@ -21,7 +25,13 @@ public class HelloWorldServer {
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
             response.setContentType("text/plain");
             response.setStatus(HttpServletResponse.SC_OK);
+
+            ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Asia/Jakarta"));
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, dd MMMM yyyy HH:mm:ss", java.util.Locale.forLanguageTag("id-ID"));
+            String formattedDate = now.format(formatter);
+            
             response.getWriter().println("Arif Rahman Habibie");
+            response.getWriter().println(formattedDate);
         }
     }
 }
